5805587e88ed663638083361a4cf26cd59b3f603
apix <apixon@outlook.com>
2025-03-29 21:54:50 +0100

    added logo/branding

---
+++ src/routes/image/+layout.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <style>
+        :root {
+            --primary: #F8A971;
+            --primary-2: #DA925E;
+        }
+    </style>
+</svelte:head>
+
+<slot/>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
-		sveltekit()
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				name: "apix tools",
+				short_name: "tools",
+				theme_color: "#3dfd8d",
+				icons: [
+					{
+						src: '/apple-touch-icon.png',
+						sizes: '180x180',
+						type: 'image/png',
+					}
+				]
+			},
+			devOptions: {
+				enabled: true
+			}
+		})
 	],
 	server: {
 		allowedHosts: true
