4a8c19d85e297bbb3dd59aed46c86549c2eb3923
apix <apixon@outlook.com>
2025-03-30 11:41:15 +0200

    fix prerendering failing social embed

--- src/components/OpenGraphHead.svelte
+++ src/components/OpenGraphHead.svelte
@@ -1,26 +1,32 @@
 <script>
+    import { browser } from '$app/environment';
     import { page } from '$app/stores';
-    
+
     const defaultTitle = 'apix tools';
     const defaultDescription = "A set of web tools I created for things I needed to do at some point, and couldn't do easily.";
 
     export let title = null;
     export let description = null;
 
-    $: currentUrl = new URL($page.url).origin;
-    $: image = `${currentUrl}/social.jpg`;
+    const envBaseUrl = import.meta.env.VITE_BASE_URL || 'https://apixtools.vercel.app'; // Fallback if env is missing
+
+    let currentUrl = browser ? new URL($page.url).origin : envBaseUrl;
+    let image = `${currentUrl}/social.jpg`;
+
     $: actualTitle = title || defaultTitle;
     $: actualDescription = description || defaultDescription;
 </script>
 
 <svelte:head>
+    <!-- Social (embed) properties -->
+    <meta property="description" content={actualDescription} />
+    <!-- Open Graph -->
     <meta property="og:title" content={actualTitle} />
     <meta property="og:description" content={actualDescription} />
     <meta property="og:image" content={image} />
     <meta property="og:url" content={currentUrl} />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="apix tools">
-    
     <!-- Twitter Card tags -->
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content={actualTitle} />
